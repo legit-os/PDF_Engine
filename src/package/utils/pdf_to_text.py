b9f4dfdb5c8ee3b2ea3fb14953831002f15f9ac0
legit-os <himanshu2005sanjay@gmail.com>
2026-02-24 18:58:48 +0530

    text extraction improved

--- src/package/utils/pdf_to_text.py
+++ src/package/utils/pdf_to_text.py
@@ -1,21 +1,30 @@
-from pypdf import PdfReader
+# from pypdf import PdfReader
+# from io import BytesIO
+
+
+# def extract_text_from_pdf_bytes(pdf_bytes: bytes) -> str:
+#     try:
+#         reader = PdfReader(BytesIO(pdf_bytes))
+
+#         extracted_text = []
+
+#         for page in reader.pages:
+#             text = page.extract_text()
+#             if text:
+#                 extracted_text.append(text)
+
+#         full_text = "\n".join(extracted_text).strip()
+
+#         return full_text if full_text else ""
+
+#     except Exception:
+#         return ""
+
 from io import BytesIO
+from pdfminer.high_level import extract_text
 
-
-def extract_text_from_pdf_bytes(pdf_bytes: bytes) -> str:
+def extract_text_from_pdf_bytes(pdf_bytes):
     try:
-        reader = PdfReader(BytesIO(pdf_bytes))
-
-        extracted_text = []
-
-        for page in reader.pages:
-            text = page.extract_text()
-            if text:
-                extracted_text.append(text)
-
-        full_text = "\n".join(extracted_text).strip()
-
-        return full_text if full_text else ""
-
-    except Exception:
+        return extract_text(BytesIO(pdf_bytes)).strip()
+    except:
         return ""

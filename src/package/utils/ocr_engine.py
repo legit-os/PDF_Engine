adcd8d30ad2ac0925a0ea3514fb085f1366eb80c
legit-os <himanshu2005sanjay@gmail.com>
2026-02-24 20:42:01 +0530

    version update

--- src/package/utils/ocr_engine.py
+++ src/package/utils/ocr_engine.py
@@ -205,7 +205,7 @@ def apply_paddle_docker_ocr(pages: list):
     if not eligible_pages:
         return
     
-    time_estimate = len(eligible_pages)*15 + len(eligible_pages)*5
+    time_estimate = len(eligible_pages)*15 + len(eligible_pages)*5 + 300
     
     streamlit.info(f"Processing {len(eligible_pages)} pages, Estimated Time=> {time_estimate//3600} hours , {time_estimate//60 - (time_estimate//3600)*60} minutes")
     
@@ -232,7 +232,7 @@ def apply_paddle_docker_ocr(pages: list):
                 "legitos/paddleocr-docker:v1",
             ],
             check=True,
-            timeout=time_estimate + 300
+            timeout=time_estimate
         )
         streamlit.text("OCR done, Creating pdf pages...")
         for page in eligible_pages:
